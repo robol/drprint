f47cc0bca2d3458ef403cd58bb4f30aaa93ec5cb
Leonardo <leo@robol.it>
2010-06-25 10:43:32 +0200

    Tolto l'archivio debian. Da ora in poi pacchettizzerò da un'altra parte.

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the drprint package
-#
-0 4	* * *	root	[ -x /usr/bin/drprint_maintenance ] && /usr/bin/drprint_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(drprint):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="drprint" command="/usr/bin/drprint"
